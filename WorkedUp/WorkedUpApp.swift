32f68163f2b3514f10fc04e4823a8e213d9031ce
Michel Storms <stormychel@me.com>
2023-03-01 18:43:48 +0100

    - struggling with file access

--- WorkedUp/WorkedUpApp.swift
+++ WorkedUp/WorkedUpApp.swift
@@ -10,6 +10,37 @@ import SwiftUI
 @main
 struct WorkedUpApp: App {
     @State var currentNumber: String = "1"
+    
+    init() {
+        
+        // just trying to get to the data we need first
+        
+//        if let filepath = Bundle.main.path(forResource: "example", ofType: "txt") {
+        
+        let urlString = "file:///Users/stormychel/Library/Application Support/Upwork/Upwork/Logs/upwork..20230301.log"
+        
+        print("urlString: \(urlString)")
+        print("test     : file:///Users/stormychel/Library/Containers/com.tiseyer.typegear-ai/Data/Documents/")
+        
+        if let url = URL(string: urlString) {
+            
+            print("URL: \(url.absoluteString)")
+            
+            do {
+                let contents = try String(contentsOfFile: url.absoluteString)
+                
+                print(contents)
+            } catch {
+                // contents could not be loaded
+                print("CATCH")
+            }
+        } else {
+            // example.txt not found!
+            print("ELSE")
+        }
+        
+        
+    }
 
     var body: some Scene {
         MenuBarExtra(currentNumber, systemImage: "\(currentNumber)") {
