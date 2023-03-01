8d083425dbb00e686a4d8ed448f053b4ae7c624c
Michel Storms <stormychel@me.com>
2023-03-01 16:26:55 +0100

    Initial Commit

---
+++ WorkedUp/WorkedUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkedUpApp.swift
+//  WorkedUp
+//
+//  Created by Michel Storms on 01/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct WorkedUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
