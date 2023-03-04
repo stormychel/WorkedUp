797a0c9180d13e0d15e884784d165034981250f0
Michel Storms <stormychel@me.com>
2023-03-04 01:22:24 +0100

    - working

--- WorkedUp/WorkedUpApp.swift
+++ WorkedUp/WorkedUpApp.swift
@@ -27,11 +27,66 @@ struct WorkedUpApp: App {
                 print("urlString: \(urlString)")
                     
                 do {
-                    let newestLogFileString = try String(contentsOfFile: urlString)
+                    let newestLogFileString = try String(contentsOfFile: urlString) // make string from file
                     
-                    print("newestLogFileString: \(newestLogFileString)")
+                    // chop string into array of lines
+                    var lines: [String] = [] // contains file line by line
+                    for line in newestLogFileString.split(separator: "\n") {
+                        print (line)
+                        
+                        lines.append( String(line) )
+                    }
+                    
+                    // TODO: scan for entry line containing LOAD_WORKED_TIME, this is the start of the JSON
 
-                    // TODO: extract highest count of 'minutesWorkedThisWeek' and do something with it
+                    var contracts: [String : String] = [ : ] // contains rollupId entries
+                    
+                    var currenrRollupId: String = ""
+                    
+                    for line in lines {
+                        if line.contains("rollupId") {
+                            currenrRollupId = line.filter("0123456789".contains)
+                        } else if line.contains("minutesWorkedThisWeek") {
+                            if !currenrRollupId.isEmpty {
+                                contracts[currenrRollupId] = line.filter("0123456789".contains)
+                                currenrRollupId = ""
+                            }
+                        }
+                    }
+                    
+                    print(contracts)
+                    
+                    /*
+                     [2023-03-03T23:01:27.057] [INFO] fe.[default] - [INFO] <Redux> - action { type: 'LOAD_WORKED_TIME',
+                       payload:
+                        [ { rollupId: '30472057',
+                            minutesWorkedThisWeek: 0,
+                            minutesWorkedToday: 0,
+                            lastTimeWorked: 1675342994 },
+                          { rollupId: '31147095',
+                            minutesWorkedThisWeek: 0,
+                            minutesWorkedToday: 0,
+                            lastTimeWorked: 1677414806 },
+                          { rollupId: '31592247',
+                            minutesWorkedThisWeek: 0,
+                            minutesWorkedToday: 0,
+                            lastTimeWorked: 1669898652 },
+                          { rollupId: '31744019',
+                            minutesWorkedThisWeek: 0,
+                            minutesWorkedToday: 0,
+                            lastTimeWorked: 1673558939 },
+                          { rollupId: '32175518',
+                            minutesWorkedThisWeek: 0,
+                            minutesWorkedToday: 0,
+                            lastTimeWorked: 0 },
+                          { rollupId: '32614295',
+                            minutesWorkedThisWeek: 1510,
+                            minutesWorkedToday: 410,
+                            lastTimeWorked: 1677861093 } ] }
+                     */
+                    
+                    
+                    
                     
                 } catch {
                     print(error)
